12e6b86ea2f60770c8b0fabd70a068440c4776c1
TobCraft3521 <tobiashackenberg@outlook.de>
2025-03-04 18:19:45 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config"
+import { Client } from "discord.js"
+import { syncCommands } from "./slash-cmds/init"
+
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "GuildMembers", "MessageContent"],
+})
+
+syncCommands()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`)
+})
+
+client.login(process.env.TOKEN)
